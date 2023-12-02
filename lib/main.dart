53a3748c849e3526fa195928d99fe245f4dc6a4c
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-02 12:54:54 +0530

    Learnig About Alignment and lists

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,22 @@ void main() {
     MaterialApp(
       // it the root widget for flutter
       // it is constructor fuction of a class , also it is widget
-      home:Scaffold(// Named Parameters 
-      backgroundColor: Colors.purple[300],// here colors is a object provided by the colors class and Purple is a property(better to say it is list here because [300],we can also use fromARGB()  ), it returns a object of COLOR OBJECT , It is also called Configurational Object 
+      home: Scaffold(
+        // Named Parameters
+        // backgroundColor: Colors.purple[300],// here colors is a object provided by the colors class and Purple is a property(better to say it is list here because [300],we can also use fromARGB()  ), it returns a object of COLOR OBJECT , It is also called Configurational Object
         body: Container(
+          // using Const helps in memory Optimization
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 149, 61, 165),
-              Color.fromARGB(223, 251, 110, 110),
-            ]),
+            // Here this returns BoxDecoration Object which is also of type DECORATION OBJECT
+            gradient: LinearGradient(
+              colors: [
+                // List a (generic type) is created by using [] with comma seperated values
+                Color.fromARGB(255, 108, 9, 126),
+                Color.fromARGB(223, 99, 16, 116),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
           child: const Center(
             child: Text("hello world"),
