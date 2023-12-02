c525162e0a51f9072e26476883a3cdb3d8bc46d2
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-02 18:53:26 +0530

    Adding TextButton and its styling and learning about sized box

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -68,6 +68,7 @@ class GradientContainer extends StatelessWidget {
       : color1 = Colors.purple,
         color2 = const Color.fromARGB(
             255, 111, 20, 127); // this this the multiple constructor function
+  void rollDice() {}
 
   final Color color1;
   final Color color2;
@@ -92,9 +93,25 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice1.png',
-          width: 200,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice1.png',
+              width: 200,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                onPressed: rollDice,
+                style: TextButton.styleFrom(
+                    // for adding padding we can use padding or by using sized box
+                    // padding: const EdgeInsets.only(top: 20),
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(fontSize: 28)),
+                child: const Text("Roll Dice"))
+          ],
         ), // here we are using the multiple constructor function of the image class
       ),
     );
