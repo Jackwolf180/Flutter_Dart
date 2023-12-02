edec76be8890a5e47da5f571d3c4c6cab52e1590
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-02 12:15:30 +0530

    Learning about scaffold , Types , Const and other basics Fuldamental

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home:
-          Scaffold(body: Container(decoration: const BoxDecoration(gradient: LinearGradient(colors: [ Color.fromARGB(255, 149, 61, 165),Color.fromARGB(223, 251, 110, 110),])),child: const Center(child: Text("hello world")))),
+      // it the root widget for flutter
+      // it is constructor fuction of a class , also it is widget
+      home:Scaffold(// Named Parameters 
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 149, 61, 165),
+              Color.fromARGB(223, 251, 110, 110),
+            ]),
+          ),
+          child: const Center(
+            child: Text("hello world"),
+          ),
+        ),
+      ),
     ),
   );
 }
