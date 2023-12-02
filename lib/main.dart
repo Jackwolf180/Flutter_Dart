d186705f2db0c53a9a2ae94161bb2694f5a4889e
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-02 12:25:04 +0530

    Understanding Configurartional Object

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
       // it the root widget for flutter
       // it is constructor fuction of a class , also it is widget
       home:Scaffold(// Named Parameters 
+      backgroundColor: Colors.purple[300],// here colors is a object provided by the colors class and Purple is a property(better to say it is list here because [300],we can also use fromARGB()  ), it returns a object of COLOR OBJECT , It is also called Configurational Object 
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(colors: [
