4abc72d4c9c8921d1045f96572cd937d3f8719b3
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-05 14:27:49 +0530

    creating Elevated button

---
+++ lib/answer_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {super.key, required this.onClicked, required this.answer});
+  final void Function() onClicked;
+  final String answer;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onClicked,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: const Color.fromARGB(255, 86, 4, 101),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        ),
+        child: Text(answer));
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:basics/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -11,6 +12,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Dummy data");
+    return SizedBox(
+      width: double
+          .infinity, // this is another method insted of using center inside a container
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "The Question",
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(onClicked: () {}, answer: "answer1"),
+          AnswerButton(onClicked: () {}, answer: "answer2"),
+          AnswerButton(onClicked: () {}, answer: "answer3"),
+          AnswerButton(onClicked: () {}, answer: "answer4"),
+        ],
+      ),
+    );
   }
 }
