5be7a2ea29c2d20770c2133ced53f2753629934a
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-05 10:47:45 +0530

    Using Ternary Operator for lifting State

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,21 +10,58 @@ class Quiz extends StatefulWidget {
   }
 }
 
+
+// METHOD 1 OF LIFTING STATE 
+
+// class _QuizState extends State<Quiz> {
+//   // we here add _ to donate the this is a private class hence this class is only accessible to Quiz class
+
+//   Widget?
+//       activeScreen; // here we use Widged insted of var bacause var is too specific as activeScreen will have type of widget but also of type StartScreen and will throw an error on using it in setState for changing the screen. Thus we use Widged which is less specific hence more preferrable
+//   @override
+//   void initState() {
+//     // using the inti state method . this method initializes the the switch screen method and then sets the active screen widgedt to startscreen(swithchScreen) and then runs the (flow is like _QuizState(constructor)-> initializing switchScreen -> initState()-> build of )
+//     activeScreen = StartScreen(switchScreen);
+//     super.initState();
+//   }
+
+//   void switchScreen() {
+//     setState(() {
+//       activeScreen = const QuestionsScreen();
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Container(
+//           // here we kept the container here because we wanted to keep the same style in all the pages
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//                 colors: [Colors.purple, Color.fromARGB(255, 106, 11, 123)],
+//                 begin: Alignment.topLeft,
+//                 end: Alignment.bottomRight),
+//           ),
+//           child: activeScreen,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+// METHOD 2 OF LIFTING STATE
+
 class _QuizState extends State<Quiz> {
   // we here add _ to donate the this is a private class hence this class is only accessible to Quiz class
-
-  Widget?
-      activeScreen; // here we use Widged insted of var bacause var is too specific as activeScreen will have type of widget but also of type StartScreen and will throw an error on using it in setState for changing the screen. Thus we use Widged which is less specific hence more preferrable
-  @override
-  void initState() {
-    // using the inti state method . this method initializes the the switch screen method and then sets the active screen widgedt to startscreen(swithchScreen) and then runs the (flow is like _QuizState(constructor)-> initializing switchScreen -> initState()-> build of )
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = "currentScreen";
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = "QuizScreen";
     });
   }
 
@@ -40,7 +77,7 @@ class _QuizState extends State<Quiz> {
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight),
           ),
-          child: activeScreen,
+          child: activeScreen=="CurrentScreen"?StartScreen(switchScreen):const QuestionsScreen()
         ),
       ),
     );
