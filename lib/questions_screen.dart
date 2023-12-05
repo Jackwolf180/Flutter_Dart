50d744495a6a22aac1f524c3d8e7d80190b2b691
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-05 15:04:35 +0530

    Aligning The answers in the question at center

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -19,20 +19,25 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     return SizedBox(
       width: double
           .infinity, // this is another method insted of using center inside a container
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(onClicked: () {}, answer: answer);
-          })
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(onClicked: () {}, answer: answer);
+            })
+          ],
+        ),
       ),
     );
   }
