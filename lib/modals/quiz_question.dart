1665c76eb6826fe7b3604ff525b5d00a5107aa16
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-05 15:08:26 +0530

    Mutating the values in memory (using shuffle to mutated the valued in place in memory)

--- lib/modals/quiz_question.dart
+++ lib/modals/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffeledAnswers = List.of(answers);
+    shuffeledAnswers.shuffle();
+    return shuffeledAnswers;
+  }
 }
