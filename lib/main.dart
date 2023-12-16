992fc78a65c3fa6fc18a56e46fa8315161c0d347
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-17 00:05:38 +0530

    Changing the theme of different elements using the theme properties of the theme data

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,28 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData().copyWith(
-      colorScheme: kColorScheme,// here we are creating the colorScheme of the entire app 
-      appBarTheme: const AppBarTheme().copyWith( // here copy with overRides some selected properties of the intiial theme
-        backgroundColor: kColorScheme.onPrimaryContainer,
-        foregroundColor: kColorScheme.primaryContainer
-
-      )
-
+      colorScheme:
+          kColorScheme, // here we are creating the colorScheme of the entire app
+      appBarTheme: const AppBarTheme().copyWith(
+          // here copy with overRides some selected properties of the intiial theme
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+            // titleLarge: ThemeData().textTheme.titleLarge.copyWith() //one of the approach is this
+            titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14),
+          ),
     ), // using the themeing system to theme the etire app
     home: const Expenses(),
   ));
