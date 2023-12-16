84acf7c2f7495bc35e7ff893028865e33382774f
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-16 23:41:17 +0530

    Theme introduction

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:basics/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Expenses(),
+    theme: ThemeData().copyWith(scaffoldBackgroundColor: Color.fromARGB(223, 173, 105, 209)),// using the themeing system to theme the etire app 
+    home: const Expenses(),
   ));
 }
