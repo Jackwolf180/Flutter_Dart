07fb37cdce5ae0812798e86365d0cf3b41ba839c
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-16 23:52:35 +0530

    Copywith function for themeing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:basics/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 96, 59,
+        181)); // for creating scheme related variables for creating the scheme
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData().copyWith(scaffoldBackgroundColor: Color.fromARGB(223, 173, 105, 209)),// using the themeing system to theme the etire app 
+    theme: ThemeData().copyWith(
+      colorScheme: kColorScheme,// here we are creating the colorScheme of the entire app 
+      appBarTheme: const AppBarTheme().copyWith( // here copy with overRides some selected properties of the intiial theme
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer
+
+      )
+
+    ), // using the themeing system to theme the etire app
     home: const Expenses(),
   ));
 }
