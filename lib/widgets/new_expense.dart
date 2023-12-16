c444e9f7ac4c73b4bbcee371d79736c10b888935
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-16 11:59:50 +0530

    Using the showDatePicker modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,6 +10,15 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+        context: context,
+        firstDate: firstDate,
+        lastDate: now,
+        initialDate: now);
+  }
 
   @override
   void dispose() {
@@ -34,19 +43,40 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text("Title"),
             ),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: "\$",
-              label: Text("Amount"),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: "\$",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Selected Date"),
+                  IconButton(
+                      onPressed: _presentDatePicker, icon: const Icon(Icons.calendar_month))
+                ],
+              ))
+            ],
           ),
           Row(
             children: [
-              TextButton(onPressed: () {
-                Navigator.pop(context);
-              }, child: const Text("Cancel")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel")),
               const SizedBox(
                 width: 4,
               ),
