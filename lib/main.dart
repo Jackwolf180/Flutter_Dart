3075f6550342813edd297963d65835fc9210eca0
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-12-17 10:05:05 +0530

    Setting Up dark Theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,30 @@ import 'package:basics/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-    seedColor: const Color.fromARGB(255, 96, 59,
-        181)); // for creating scheme related variables for creating the scheme
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+); // for creating scheme related variables for creating the scheme
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        ),
+      ),
+    ),
     theme: ThemeData().copyWith(
       colorScheme:
           kColorScheme, // here we are creating the colorScheme of the entire app
@@ -32,6 +50,7 @@ void main() {
                 fontSize: 14),
           ),
     ), // using the themeing system to theme the etire app
+    themeMode: ThemeMode.system, // This is the default theme
     home: const Expenses(),
   ));
 }
